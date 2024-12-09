ad9025e0575da10dea64b4353feb6acea0d4d700
patedas <pateda.online@gmail.com>
2024-12-09 23:18:34 +0200

    contact finished

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,37 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+// // Get the modal
+// var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-}
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById("myImg");
+// var modalImg = document.getElementById("img01");
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+// }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// SUCCESS POPUP
+
+// Check for the hash on page load
+window.addEventListener("load", function () {
+    console.log(window.location.hash)
+    if (window.location.hash === "#success") {
+      // Show the popup if the hash is #success
+      document.getElementById("success").style.display = "block";
+  
+      // Clear the hash to prevent the popup from showing again on reload
+      history.replaceState(null, null, window.location.pathname);
+    }
+  });
+  
+  // Close popup functionality
+  document.getElementById("close-popup").addEventListener("click", function () {
+    document.getElementById("success").style.display = "none";
+  });
